6b61489884d8969280dd91a2a6436ac8442c8897
joope <pyry.joona@helsinki.fi>
2017-08-20 19:46:18 +0300

    Add color picker

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 const db = redis.createClient();
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
