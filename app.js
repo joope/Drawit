ab702d355588df80a8ccbe8d8acb553f8df91a63
joope <pyry.joona@helsinki.fi>
2017-08-24 21:31:44 +0300

    Change to socket.io for reliability

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const WebSocket = require('ws');
 const express = require('express');
 const redis = require('redis');
 const path = require('path');
+
 const db = redis.createClient();
 const app = express();
+const server = require('http').Server(app);  
+const io = require('socket.io')(server);
+
 
 app.use(express.static('public'));
 
@@ -15,31 +18,28 @@ db.on('connect', function(){
     console.log('connected to redis');
 });
 
-const wss = new WebSocket.Server({ port: 8082 });
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    d = JSON.parse(data);
-    if(d.type === 'image') {
-      db.set('image', d.data, function(){
-      })
-    }
-    
-    if(d.type === 'stroke') {      
-      wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-      });
-    }
-  });
-  db.get('image', function(err, reply){
-    var init = {};
-    init.type = 'init';
-    init.data = reply;
-    ws.send(JSON.stringify(init));
+io.on('connection', function connection(client) {
+  console.log(client.id + ' connected');
+  
+  db.get('image', function(err, reply) {
+    client.emit('init', reply);
   })
+
+  client.on('save', function incoming(data) {
+    db.set('image', data, function(){
+    })
+  })
+  
+  client.on('stroke', function(stroke) {
+    // do some checks here
+    client.broadcast.emit('stroke', stroke);
+  })
+  
+  client.on('disconnect', function(){
+    console.log(client.id + ' disconnected');
+  })
+
 });
 
 
-app.listen(8080);
+server.listen(8080);
